79692207c367845c8413acffb5d9d5a9a0f41746
npiscitelloND <npiscitello@nextdroidsystems.com>
2017-11-27 20:23:58 -0500

    Database is lined up; just need to figure out how to eventify it...

--- HatTrickGUI/Main.cs
+++ HatTrickGUI/Main.cs
@@ -17,12 +17,23 @@ namespace HatTrickGUI
         {
             InitializeComponent();
 
-            List<string> tables = new List<string>();
+            Dictionary<string, DataTable> tables = new Dictionary<string, DataTable>();
             foreach( DataTable table in ds.Tables )
             {
-                tables.Add(table.ToString());
+                tables.Add(table.ToString(), table);
             }
-            part_type_combobox.DataSource = tables;
+            part_type_combobox.DataSource = tables.Keys.ToArray();
+
+            Dictionary<string, DataRow> rows = new Dictionary<string, DataRow>();
+            if (tables.ContainsKey(part_type_combobox.SelectedText))
+            {
+                foreach (DataRow row in tables[part_type_combobox.SelectedText].Rows)
+                {
+                    rows.Add(row.ToString(), row);
+                }
+            }
+            nxdn_combobox.DataSource = rows.Keys.ToArray();
         }
+
     }
 }
